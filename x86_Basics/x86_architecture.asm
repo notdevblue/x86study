4c17032ee1d6bea4383ab448c7a0ab3b68ca57e4
HanNgle <wooyeophan@gmail.com>
2024-05-29 18:32:46 +0900

    six segments (only lists)

--- x86_Basics/x86_architecture.asm
+++ x86_Basics/x86_architecture.asm
@@ -3,18 +3,17 @@
 ; x86 아기택처는 8개의 General-Purpose Register (GPR), 6개의 Segment Register, 1개의 Flags Register와 Instruction Pointer 가 있음.
 ; x86 의 64비트 버전은 추가적인 레지스터가 있음.
 
-; GPR 16비트 명명 규칙
+; ## GPR 16비트 명명 규칙
 
 ; 8개의 GPR은 아레와 같음
-; 1. Accumulator(누산기) Register (AX). 산술 연산에 사용됨
-;    상수를 합쳐서 누산기에 넣는 연산코드는 1바이트임.
-; 2. Base register (BX). 데이터를 향한 포인터로 사용됨. (Segmented mode 일 때는 Segment register DS 에 위치함)
-; 3. Couter register (CX). 명령을 이동/회전, 반복할때 사용됨.
-; 4. Stack Pointer register (SP). 스택 맨 위를 향한 포인터.
-; 5. Stack Base Pointer register (BP). 스택 맨 아레 (기반)을 향한 포인터.
-; 6. Destination Index register (DI). stream 작업의 목적지를 향한 포인터.
-; 7. Source Index register (SI). stream 작업의 원본을 향한 포인터.
-; 8. Data register (DX). 산술 연산과 입출력 연산에 사용됨.
+; 1. Accumulator(누산기) Register   (AX). 산술 연산에 사용됨. 상수를 합쳐서 누산기에 넣는 연산코드는 1바이트임.
+; 2. Base register                  (BX). 데이터를 향한 포인터로 사용됨. (Segmented mode 일 때는 Segment register DS 에 위치함)
+; 3. Couter register                (CX). 명령을 이동/회전, 반복할때 사용됨.
+; 4. Stack Pointer register         (SP). 스택 맨 위를 향한 포인터.
+; 5. Stack Base Pointer register    (BP). 스택 맨 아레 (기반)을 향한 포인터.
+; 6. Destination Index register     (DI). stream 작업의 목적지를 향한 포인터.
+; 7. Source Index register          (SI). stream 작업의 원본을 향한 포인터.
+; 8. Data register                  (DX). 산술 연산과 입출력 연산에 사용됨.
 
 ; 위에서 설명한 GPR들의 순서는 이유가 있음.
 ; push-to-stack 연산과 동일한 순서임
@@ -44,3 +43,12 @@
 ; | 16-bit	 | AX		    | BX		| CX		| SP		    | BP		            | DI		    | SI	    | DX    |
 ; | 8-bit	 | AH AL		| BH BL	    | CH CL	    | SPL		    | BPL		            | DIL		    | SIL	    | DH DL |
 
+; ## Segment registers (세그먼트 레지스터)
+
+; 6개의 세그먼트 레지스터는 아레와 같음
+; 1. Stack Segment  (SS). 스택을 향한 포인터                    (Stack 의 S)
+; 2. Code Segment   (CS). 코드를 향한 포인터                    (Code 의 C)
+; 3. Data Segment   (DS). 데이터를 향한 포인터                  (Data 의 D)
+; 4. Extra Segment  (EX). 추가 데이터를 향한 포인터             (Extra 의 E, D 다음이 E 이기도 함)
+; 5. F Segment      (FS). 추가 추가 데이터를 향한 포인터        (E 다음이 F)
+; 6. G Segment      (GS). 추가 추가 추가 데이터를 향한 포인터   (F 다음이 G)
